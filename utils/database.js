447f7e2e2474818526222c1cadd03902975700b4
PavelMGs <pavelmg99@gmail.com>
2021-07-01 19:47:35 +0300

    feat: move db to heroku

--- utils/database.js
+++ utils/database.js
@@ -1,12 +1,14 @@
 const Sequelize = require('sequelize')
 
-const DB_NAME = 'blog';
-const USER_NAME = 'admin';
-const PASSWORD = '12345678';
+const DB_NAME = 'mg-blog-db-connection';
+const USER_NAME = 'b54afa87289859';
+const PASSWORD = '6f8139f1';
+// 12345678-Qwerty
+// mysql://b54afa87289859:6f8139f1@eu-cdbr-west-01.cleardb.com/heroku_2ac31d881f3efc4?reconnect=true
 
 
-const sequelize = new Sequelize(DB_NAME, USER_NAME, PASSWORD, {
-    host: 'localhost',
+const sequelize = new Sequelize('mysql://b54afa87289859:6f8139f1@eu-cdbr-west-01.cleardb.com/heroku_2ac31d881f3efc4?reconnect=true', {
+    host: 'eu-cdbr-west-01.cleardb.com',
     dialect: 'mysql',
 });
 
