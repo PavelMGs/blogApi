99ef9909f24f47dbe5ee4557343886a4388019d0
PavelMGs <pavelmg99@gmail.com>
2021-07-26 14:23:41 +0300

    fix: ry to fix db

--- utils/database.js
+++ utils/database.js
@@ -1,11 +1,25 @@
 const Sequelize = require('sequelize')
 
+const DB_NAME = 'mg-blog-db-connection';
+const USER_NAME = 'b54afa87289859';
+const PASSWORD = '6f8139f1';
+
+
+// const DB_NAME = 'blog';
+// const USER_NAME = 'admin';
+// const PASSWORD = '12345678';
+
+// 12345678-Qwerty
+
 
 const sequelize = new Sequelize('mysql://b54afa87289859:6f8139f1@eu-cdbr-west-01.cleardb.com/heroku_2ac31d881f3efc4?reconnect=true', {
     host: 'eu-cdbr-west-01.cleardb.com',
     dialect: 'mysql',
 });
 
-
+// const sequelize = new Sequelize( DB_NAME, USER_NAME, PASSWORD, {
+//     host: 'localhost',
+//     dialect: 'mysql',
+// });
 
 module.exports = sequelize;
