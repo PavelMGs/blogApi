6b5b0a2c7cb8ef9773c2286c49b4a33d27c40f4b
PavelMGs <pavelmg99@gmail.com>
2021-07-26 14:18:57 +0300

    feat: switch body to long text

--- models/post.js
+++ models/post.js
@@ -13,7 +13,7 @@ const post = sequelize.define('Post', {
         allowNull: false
     },
     body: {
-        type: Sequelize.STRING,
+        type: Sequelize.TEXT('long'),
         allowNull: false
     },
 })
